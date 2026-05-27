7e4e9d455cf8c00425544a5760c9660ea809a756
Quang Quoc <quangquoc666@gmail.com>
2026-05-27 19:47:06 +0700

    feat: install and configure Nuxt UI with Tailwind CSS

---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <UButton label="Click me" />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
 
   typescript: {
     typeCheck: true,
   },
-
-  modules: ['@nuxt/eslint'],
   eslint: {
     config: {
-      stylistic: true
-    }
-  }
+      stylistic: true,
+    },
+  },
 })
