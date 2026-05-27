1929e87c49a449080f1f33482d390c62a5fa84e4
Quang Quoc <quangquoc666@gmail.com>
2026-05-27 19:38:39 +0700

    feat: integrate @nuxt/eslint and enable TypeScript type checking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+
+  typescript: {
+    typeCheck: true,
+  },
+
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
