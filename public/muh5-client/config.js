1a0ad47417a1f03170829fb1f54e1fda7ce788b0
Quang Quoc <quangquoc666@gmail.com>
2026-06-02 03:53:07 +0700

    Restore MUH5 lightweight client assets

---
+++ public/muh5-client/config.js
@@ -0,0 +1,32 @@
+// Game Configuration for MUH5 Legacy Client
+(function () {
+    var urlParam = {};
+    var paraUrl = window.location.href;
+    var whIndex = paraUrl.indexOf("?");
+    if (whIndex != -1) {
+        var param = paraUrl.slice(whIndex + 1).split("&");
+        var strArr;
+        for (var i = 0; i < param.length; i++) {
+            strArr = param[i].split("=");
+            var key = strArr[0];
+            var val = strArr.slice(1).join("=");
+            try {
+                urlParam[key] = decodeURIComponent(val.replace(/\+/g, " "));
+            } catch (e) {
+                urlParam[key] = val;
+            }
+        }
+    }
+
+    // Full query map for ccgame-entrance.js (nickName, roleCount, …)
+    window["__muh5UrlParams"] = urlParam;
+
+    // Assign global variables required by Egret engine
+    window["uid"] = urlParam.user || "";
+    window["sid"] = urlParam.srvid || "1";
+    window["spverify"] = urlParam.spverify || "portal-auth";
+    window["svrip"] = urlParam.srvaddr || "muh5-ws.ccgame.org/s1/";
+    window["port"] = urlParam.srvport || "443";
+    window["loginre"] = "./login_bt.json";
+    window["showurl"] = false;
+})();
---
+++ public/muh5-client/h5/ccgame-entrance.js
@@ -0,0 +1,44 @@
+// Merge CCGame /play iframe query params into legacy urlParamDefault (CDN main.min.js).
+(function () {
+    function deriveNickFromGuestAccount(user) {
+        if (!user) {
+            return '';
+        }
+        var match = String(user).match(/^guest_([a-f0-9]{5,})/i);
+        if (!match) {
+            return '';
+        }
+        return 'g' + match[1].slice(0, 5);
+    }
+
+    function applyUrlParams() {
+        if (typeof urlParamDefault === 'undefined') {
+            return false;
+        }
+
+        var raw = window.__muh5UrlParams || {};
+
+        var nick = raw.nickName ? String(raw.nickName) : '';
+        if (!nick) {
+            nick = deriveNickFromGuestAccount(raw.user);
+        }
+        if (nick) {
+            urlParamDefault.nickName = nick;
+        }
+
+        if (typeof EntranceParam !== 'undefined' && EntranceParam.urlParam && nick) {
+            EntranceParam.urlParam.nickName = nick;
+        }
+
+        return true;
+    }
+
+    if (!applyUrlParams()) {
+        var tries = 0;
+        var timer = setInterval(function () {
+            if (applyUrlParams() || ++tries > 1000) {
+                clearInterval(timer);
+            }
+        }, 0);
+    }
+})();
